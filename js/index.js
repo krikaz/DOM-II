3ee781fa9fe938ba19b5d97cc7ae982a761c1697
krikaz <pascal.bou@protonmail.com>
2019-05-14 13:58:13 +0200

    creating 10 addEventListeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-// Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', function() {this.style.color = 'red'});
+logoHeading.addEventListener('dblclick', function() {this.style.color = 'blue'});
+
+document.addEventListener('load', console.log('Page has loaded.'));
+
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseenter', function() {this.style['background-color'] = 'green'});
+nav.addEventListener('mouseleave', function() {this.style['background-color'] = 'yellow'});
+
+const p = document.querySelectorAll('p');
+for (let i=0; i<p.length; i++) {
+  p[i].addEventListener('click', function() {this.style.color = 'pink'});
+}
+
+document.body.addEventListener('keydown', function() {this.style.color = 'purple'});
+document.body.addEventListener('keyup', function() {this.style.color = 'orange'});
+
+const h2 = document.querySelectorAll('h2');
+for (let i=0; i<h2.length; i++) {
+  // h2[i].addEventListener('mouseenter', function() {this.style.color = 'rgb(150, 150, 150)'});
+  h2[i].addEventListener('mousemove', function() {this.style.color = getRGB()});
+
+}
+
+function getRGB() {
+  const first = getRandomInt();
+  const second = getRandomInt();
+  const third = getRandomInt();
+
+  return `rgb(${first}, ${second}, ${third})`
+}
+
+function getRandomInt() {
+  return Math.floor(Math.random() * Math.floor(256));
+}
