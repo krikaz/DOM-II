1173dfdff840edb94ddf5eafd71d34f02bf7e52e
krikaz <pascal.bou@protonmail.com>
2019-05-14 14:10:26 +0200

    changed all colors from callbacks to random

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
-const logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener('mouseover', function() {this.style.color = 'red'});
-logoHeading.addEventListener('dblclick', function() {this.style.color = 'blue'});
-
 document.addEventListener('load', console.log('Page has loaded.'));
 
+document.body.addEventListener('keydown', function() {this.style.color = getRGB()});
+document.body.addEventListener('keyup', function() {this.style.color = getRGB()});
+
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', function() {this.style.color = getRGB()});
+
 const nav = document.querySelector('.nav');
-nav.addEventListener('mouseenter', function() {this.style['background-color'] = 'green'});
-nav.addEventListener('mouseleave', function() {this.style['background-color'] = 'yellow'});
+nav.addEventListener('mouseenter', function() {this.style['background-color'] = getRGB()});
+nav.addEventListener('mouseleave', function() {this.style['background-color'] = getRGB()});
 
 const p = document.querySelectorAll('p');
 for (let i=0; i<p.length; i++) {
-  p[i].addEventListener('click', function() {this.style.color = 'pink'});
+  p[i].addEventListener('click', function() {this.style.color = getRGB()});
 }
-
-document.body.addEventListener('keydown', function() {this.style.color = 'purple'});
-document.body.addEventListener('keyup', function() {this.style.color = 'orange'});
 
 const h2 = document.querySelectorAll('h2');
 for (let i=0; i<h2.length; i++) {
-  // h2[i].addEventListener('mouseenter', function() {this.style.color = 'rgb(150, 150, 150)'});
   h2[i].addEventListener('mousemove', function() {this.style.color = getRGB()});
-
 }
+
+const h4 = document.querySelectorAll('h4');
+for (let i=0; i<h4.length; i++) {
+  h4[i].addEventListener('dblclick', function() {this.style.color = getRGB()});
+}
+
+const img = document.querySelectorAll('img');
+for (let i=0; i<img.length; i++) {
+  img[i].addEventListener('contextmenu', console.log('Image is being dragged.'));
+}
+
+
+// functions
+
 
 function getRGB() {
   const first = getRandomInt();
